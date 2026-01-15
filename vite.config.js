fe3124458347c7487882ab6059b12129087e705a
Larsson Fernández <lfernandezhuaringa@gmail.com>
2026-01-14 19:50:25 -0500

    Eliminando rastros de submódulos y normalizando carpetas

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [react()], base: '/portafoliofinal/' })
+export default defineConfig({
+  plugins: [react()],
+})
